1256cdbbf5a03e91647e56b88fa9f9f0d8e7f331
Greg Roques <greg.roques@gmail.com>
2020-07-22 09:13:58 -0400

    coding practice before work

--- src/Components/FaceDetect/FaceDetect.js
+++ src/Components/FaceDetect/FaceDetect.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './FaceDetect.css';
+// import statsModal from './statsModal'
 
-const FaceDetect = ({ imageUrl, box }) => {
-  const array = Object.values(box).length > 0 ? Object.values(box) : '';
-  console.log(array);
-  return (
-    <div className="center ma">
-      <div className="absolute mt2">
-        <img id="inputimage" alt="" src={imageUrl} width="500px" heigh="auto" />
-        {array
-          ? array.map((boxes) => {
-              return (
-                <div
-                  className="bounding-box"
-                  style={{
-                    top: boxes.topRow,
-                    right: boxes.rightCol,
-                    bottom: boxes.bottomRow,
-                    left: boxes.leftCol,
-                  }}
-                ></div>
-              );
-            })
-          : null}
+class FaceDetect extends Component {
+  state = {
+    stats: '',
+    isOpen: false,
+  };
+
+  isModal = (stats) => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen,
+      stats,
+    }));
+  };
+
+  render() {
+    const { imageUrl, box } = this.props;
+    const array = Object.values(box).length > 0 ? Object.values(box) : '';
+    //console.log(array);
+    return (
+      <div>
+        <statsModal isOpen={this.state.isOpen} stats={this.state.stats} />
+        <div className="center ma">
+          <div className="absolute mt2">
+            <img id="inputimage" alt="" src={imageUrl} width="500px" heigh="auto" />
+            {array
+              ? array.map((boxes) => {
+                  return (
+                    <div
+                      className="bounding-box"
+                      onClick={() => this.isModal(boxes.stats)}
+                      style={{
+                        top: boxes.topRow,
+                        right: boxes.rightCol,
+                        bottom: boxes.bottomRow,
+                        left: boxes.leftCol,
+                      }}
+                    ></div>
+                  );
+                })
+              : null}
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default FaceDetect;
---
+++ src/Components/FaceDetect/statsModal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageSearchForm = ({ isOpen, stats }) => {
+  return isOpen ? <div></div> : null;
+};
+
+export default statsModal;
